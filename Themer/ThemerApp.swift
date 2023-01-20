654dc5ed8a787d92e3bbf7541e0369eb570c369b
Daniel Tull <dt@danieltull.co.uk>
2023-01-20 11:13:30 +0000

    Initial Commit

---
+++ Themer/ThemerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThemerApp.swift
+//  Themer
+//
+//  Created by Daniel Tull on 20/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct ThemerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
